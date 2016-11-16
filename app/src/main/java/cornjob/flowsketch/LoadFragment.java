ff280c91c1df378471a025ef89e5cdafed9a1f09
khanh.N <knguyen60@knights.ucf.edu>
2016-11-16 18:29:43 -0500

    load request ready

--- app/src/main/java/cornjob/flowsketch/LoadFragment.java
+++ app/src/main/java/cornjob/flowsketch/LoadFragment.java
@@ -1,14 +1,27 @@
 package cornjob.flowsketch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,6 +38,8 @@ public class LoadFragment extends Fragment {
         View rootview = inflater.inflate(R.layout.fragment_load, container, false);
         //User user= new User();
         //String name=user.setName();
+
+
         ArrayList<String> stringArrayList = new ArrayList<>();
         stringArrayList.add("Today Time Created");
         stringArrayList.add("Tomorrow Time Created");
