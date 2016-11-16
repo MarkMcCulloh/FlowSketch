dc8098ec9afe152830e63f040451488040d5fd61
khanh.N <knguyen60@knights.ucf.edu>
2016-11-15 20:52:00 -0500

    login, register

---
+++ app/src/main/java/cornjob/flowsketch/User.java
@@ -0,0 +1,11 @@
+package cornjob.flowsketch;
+
+/**
+ * Created by Khanh on 11/15/2016.
+ */
+
+public class User {
+    private String name;
+    private String email;
+    private String api_key;
+}
---
+++ app/src/main/java/cornjob/flowsketch/UserActivity.java
@@ -0,0 +1,7 @@
+package cornjob.flowsketch;
+
+/**
+ * Created by Khanh on 11/15/2016.
+ */
+public class UserActivity {
+}
