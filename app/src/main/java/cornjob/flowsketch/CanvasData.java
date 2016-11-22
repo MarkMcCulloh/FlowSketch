048bce02d9c0472769b01cd3d00c4d5b4b47c3ba
john <johnjohnson45@live.com>
2016-11-21 20:00:30 -0500

    Merge branch 'master' of https://github.com/MarkMcCulloh/FlowSketch into jjohn307-master
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/cornjob/flowsketch/AppSingleton.java
    #	app/src/main/java/cornjob/flowsketch/ImageGallery.java
    #	app/src/main/java/cornjob/flowsketch/LoadActivity.java
    #	app/src/main/java/cornjob/flowsketch/LoadFragment.java
    #	app/src/main/java/cornjob/flowsketch/LoginActivity.java
    #	app/src/main/java/cornjob/flowsketch/MainActivity.java
    #	app/src/main/java/cornjob/flowsketch/MyCanvas.java
    #	app/src/main/java/cornjob/flowsketch/Object.java
    #	app/src/main/java/cornjob/flowsketch/ObjectPic.java
    #	app/src/main/java/cornjob/flowsketch/SaveDialogFragment.java
    #	app/src/main/java/cornjob/flowsketch/ShapeCircle.java
    #	app/src/main/java/cornjob/flowsketch/ShapeRect.java
    #	app/src/main/java/cornjob/flowsketch/canvasFragment.java
    #	app/src/main/java/cornjob/flowsketch/objectText.java
    #	app/src/main/java/cornjob/flowsketch/object_Text.java
    #	app/src/main/res/menu/menu_main_logged_out.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/cornjob/flowsketch/CanvasData.java
@@ -0,0 +1,66 @@
+package cornjob.flowsketch;
+
+import java.io.Serializable;
+
+/**
+ * Created by Nguyen on 11/16/2016.
+ * Data to get out of a row in canvas table
+ */
+public class CanvasData implements Serializable{
+    private int cid;
+    private String canvasName;
+    public static String data = "";
+    private String date;
+
+    public CanvasData(int cid, String name, String data, String date){
+        this.cid=cid;
+        this.canvasName=name;
+        this.data=data;
+        this.date=date;
+    }
+
+    public CanvasData(int cid, String name, String date){
+        this.cid=cid;
+        this.canvasName=name;
+        this.data=null;
+        this.date=date;
+    }
+
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCanvasName() {
+        return canvasName;
+    }
+
+    public void setCanvasName(String canvasName) {
+        this.canvasName = canvasName;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+    public String toString(){
+        return  "Title: " + canvasName + "\nDate: " + date ;
+    }
+}
